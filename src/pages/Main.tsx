86569345adf2eceee33c53ae3ed63f07ad4acddf
Islom Yusupov <islom.yusupov07031997@gmail.com>
2024-06-24 17:16:36 +0500

    first commit

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,5 @@
 import {motion} from "framer-motion";
 import person from "../assets/person.svg";
-import imageBg from "../assets/bg-image-back.png";
 import ContentLayout from "../layouts/ContentLayout";
 
 const imageVariants = {
@@ -34,9 +33,6 @@ const textVariant = {
 function Main() {
     return (
         <div className="justify-center items-center text-center my-auto">
-            <motion.img animate={{opacity: 1}} initial={{opacity: 0}} transition={{duration: 0.25, ease: "easeInOut"}}
-                        alt="" src={imageBg} className="fixed w-screen h-screen top-0 left-0"
-                        style={{zIndex: "-1"}}/>
             <ContentLayout className="justify-evenly">
                 <motion.div
                     variants={imageVariants}
