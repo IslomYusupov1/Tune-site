df540719fdd1704e8dc73b5e88037f64a35ed806
Islom Yusupov <islom.yusupov07031997@gmail.com>
2024-07-10 17:41:22 +0500

    first commit

--- src/pages/mobile/MainMobile.tsx
+++ src/pages/mobile/MainMobile.tsx
@@ -1,5 +1,5 @@
 import {useRef} from 'react';
-import {motion, useScroll} from "framer-motion";
+import {motion, useScroll, useTransform} from "framer-motion";
 import person from "../../assets/person.svg";
 import down from "../../assets/down.svg";
 
@@ -7,21 +7,23 @@ function MainMobile() {
     const ref = useRef<HTMLDivElement>(null);
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ["0 1", "0.8 1"],
-        smooth: 1
+        offset: ["end end", "end start"]
     })
 
+    const opacity = useTransform(scrollYProgress, [0, 1], [1, 0.3]);
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 0.4])
+
     return (
-        <motion.div ref={ref} style={{ opacity: scrollYProgress }}
+        <motion.div ref={ref} style={{ opacity }}
                     className="h-screen bg-[url('../assets/bg-main.jpg')] object-cover bg-no-repeat bg-center w-full">
-            <motion.div style={{scale: scrollYProgress}} className="h-full relative flex flex-col justify-center">
+            <motion.div style={{scale}} className="h-full relative flex flex-col justify-center">
                 <img
                     className="2xl:w-[800px] xl:w-[600px] lg:w-[500px] md:w-[400px] w-full rotate-[35deg]"
                     src={person} alt=""/>
                 <div
                     className="absolute md:absolute md:bottom-0 bottom-[30px] w-full md:mx-[100px] flex flex-col items-center">
                     <h3
-                        className="text-start text-[22px] md:text-[30px] px-[30px] text-white w-full xl:text-[40px] 2xl:text-[48px] lg:text-[36px] ">Прогресс
+                        className="text-start text-[22px] mb-5 md:text-[30px] px-[30px] text-white w-full xl:text-[40px] 2xl:text-[48px] lg:text-[36px] ">Прогресс
                         не случайность
                     </h3>
                     <img className="text-center md:hidden item-center" src={down} width={32} alt=""/>
